6e6c6b3f59ce9bd6ca2313b8284cbc9fa37f0fbc
Yeabsera Kebede <yeabserak@gmail.com>
2015-06-09 11:40:29 -0400

    Initial Setup of Grapher Script

---
+++ data_input_grapher/data_input_grapher/grapher.py
@@ -0,0 +1,14 @@
+data_file=open('/Users/yeabserak/Documents/Merit/Code/nextenergy/data_input_grapher/data_input_grapher/20150609/20150609_10.txt','r')
+
+
+
+
+data_file.readline()
+
+
+
+
+
+
+
+data_file.close()
